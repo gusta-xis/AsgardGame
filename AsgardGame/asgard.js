fd047bfbc9dcd1ca8d986ccc7248844a09b7a5c6
gusta-xis <devdamascena@gmail.com>
2025-09-27 09:36:27 -0300

    add de personagem - heimdall

--- AsgardGame/asgard.js
+++ AsgardGame/asgard.js
@@ -48,89 +48,105 @@ const loki = new Character(
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZdhaFgjYZWXT8Kun3aVbnU27vN7hK4tWNNg&s'
 );
 
+const heimdall = new Character(
+    'Heimdall, o Vigilante de Asgard',
+    'Você possui a visão aguçada e a vigilância de Heimdall, o guardião da Bifrost. Sua percepção é incomparável e você está sempre atento a qualquer ameaça. Você é leal e protetor, sempre pronto para defender Asgard.',
+    'https://th.bing.com/th/id/R.558e9c6657eda9d7a1f3662b800dc1c4?rik=gdzrfFAYiDLd7A&pid=ImgRaw&r=0'
+);
+
 // Array de personagens
-characters = [frigg, thor, loki];
+characters = [frigg, thor, loki, heimdall];
 
 // Perguntas do questionário com pontuações
 const questions = [
     {
         text: 'Em uma batalha, qual seria a sua abordagem?',
         options: [
-            { text: 'Procurar uma solução diplomática para evitar o conflito.', points: { 'Frigg': 3, 'Thor': 1, 'Loki': 2 } },
-            { text: 'Avançar com força total para esmagar os inimigos.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 2 } },
-            { text: 'Usar truques e ilusões para confundir o oponente.', points: { 'Frigg': 2, 'Thor': 1, 'Loki': 3 } }
+            { text: 'Procurar uma solução diplomática para evitar o conflito.', points: { 'Frigg': 3, 'Thor': 1, 'Loki': 2, 'Heimdall': 1 } },
+            { text: 'Avançar com força total para esmagar os inimigos.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 2, 'Heimdall': 1 } },
+            { text: 'Usar truques e ilusões para confundir o oponente.', points: { 'Frigg': 2, 'Thor': 1, 'Loki': 3, 'Heimdall': 1 } },
+            { text: 'Monitorar a ameaça de longe, esperando o momento exato para soar o alarme.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 1, 'Heimdall': 3 } }
         ]
     },
     {
         text: 'Qual artefato você buscaria nos Nove Reinos?',
         options: [
-            { text: 'O Anel de Andvaranaut, que garante riqueza infinita.', points: { 'Frigg': 2, 'Thor': 1, 'Loki': 3 } },
-            { text: 'O Mjolnir, o martelo capaz de destruir montanhas.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 2 } },
-            { text: 'O Fuso de Völundr, que pode tecer o destino.', points: { 'Frigg': 3, 'Thor': 2, 'Loki': 1 } }
+            { text: 'O Anel de Andvaranaut, que garante riqueza infinita.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 3, 'Heimdall': 1 } },
+            { text: 'O Mjolnir, o martelo capaz de destruir montanhas.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 2, 'Heimdall': 1 } },
+            { text: 'O Fuso de Völundr, que pode tecer o destino.', points: { 'Frigg': 3, 'Thor': 2, 'Loki': 1, 'Heimdall': 1 } },
+            { text: 'O Gjallarhorn, o chifre cujo som alerta todos os reinos.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 1, 'Heimdall': 3 } }
         ]
     },
     {
         text: 'Sua maior virtude é...',
         options: [
-            { text: 'A paciência e a capacidade de planejar com antecedência.', points: { 'Frigg': 3, 'Thor': 2, 'Loki': 1 } },
-            { text: 'A lealdade e a coragem inabalável.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 2 } },
-            { text: 'A adaptabilidade e o pensamento rápido.', points: { 'Frigg': 2, 'Thor': 1, 'Loki': 3 } }
+            { text: 'A paciência e a capacidade de planejar com antecedência.', points: { 'Frigg': 3, 'Thor': 2, 'Loki': 1, 'Heimdall': 2 } },
+            { text: 'A lealdade e a coragem inabalável.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 1, 'Heimdall': 1 } },
+            { text: 'A adaptabilidade e o pensamento rápido.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 3, 'Heimdall': 1 } },
+            { text: 'A vigilância ininterrupta e a antecipação de eventos.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 1, 'Heimdall': 3 } }
         ]
     },
     {
         text: 'Qual seria seu papel em Asgard?',
         options: [
-            { text: 'Conselheiro e guardião dos segredos divinos.', points: { 'Frigg': 3, 'Thor': 1, 'Loki': 2 } },
-            { text: 'Protetor dos Nove Reinos, liderando exércitos.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 2 } },
-            { text: 'O artista e contador de histórias, que inspira e confunde.', points: { 'Frigg': 2, 'Thor': 1, 'Loki': 3 } }
+            { text: 'Conselheiro e guardião dos segredos divinos.', points: { 'Frigg': 3, 'Thor': 1, 'Loki': 2, 'Heimdall': 1 } },
+            { text: 'Protetor dos Nove Reinos, liderando exércitos.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 1, 'Heimdall': 1 } },
+            { text: 'O artista e contador de histórias, que inspira e confunde.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 3, 'Heimdall': 1 } },
+            { text: 'O sentinela da Bifröst, o primeiro a ver o perigo.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 1, 'Heimdall': 3 } }
         ]
     },
     {
         text: 'Você prefere resolver um problema...',
         options: [
-            { text: 'Estudando todas as variáveis até encontrar a solução ideal.', points: { 'Frigg': 3, 'Thor': 2, 'Loki': 1 } },
-            { text: 'Usando sua força e poder para superá-lo.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 2 } },
-            { text: 'Com uma solução inesperada e criativa.', points: { 'Frigg': 2, 'Thor': 1, 'Loki': 3 } }
+            { text: 'Estudando todas as variáveis até encontrar a solução ideal.', points: { 'Frigg': 3, 'Thor': 1, 'Loki': 1, 'Heimdall': 2 } },
+            { text: 'Usando sua força e poder para superá-lo.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 1, 'Heimdall': 1 } },
+            { text: 'Com uma solução inesperada e criativa.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 3, 'Heimdall': 1 } },
+            { text: 'Observando e esperando, agindo com precisão no momento certo.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 1, 'Heimdall': 3 } }
         ]
     },
     {
         text: 'Seu passatempo favorito seria...',
         options: [
-            { text: 'Aprender sobre o passado dos deuses e dos reinos.', points: { 'Frigg': 3, 'Thor': 1, 'Loki': 2 } },
-            { text: 'Treinar suas habilidades de combate e sua força.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 2 } },
-            { text: 'Inventar novos truques ou planejar uma pegadinha.', points: { 'Frigg': 2, 'Thor': 1, 'Loki': 3 } }
+            { text: 'Aprender sobre o passado dos deuses e dos reinos.', points: { 'Frigg': 3, 'Thor': 1, 'Loki': 2, 'Heimdall': 1 } },
+            { text: 'Treinar suas habilidades de combate e sua força.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 1, 'Heimdall': 1 } },
+            { text: 'Inventar novos truques ou planejar uma pegadinha.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 3, 'Heimdall': 1 } },
+            { text: 'Ficar de guarda, alerta, mesmo quando o perigo parece distante.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 1, 'Heimdall': 3 } }
         ]
     },
     {
         text: 'Qual é a sua principal motivação?',
         options: [
-            { text: 'O desejo de proteger o conhecimento e a ordem.', points: { 'Frigg': 3, 'Thor': 2, 'Loki': 1 } },
-            { text: 'O dever de proteger os fracos e a honra da sua família.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 2 } },
-            { text: 'A busca por desafios e a emoção de ser imprevisível.', points: { 'Frigg': 2, 'Thor': 1, 'Loki': 3 } }
+            { text: 'O desejo de proteger o conhecimento e a ordem.', points: { 'Frigg': 3, 'Thor': 2, 'Loki': 1, 'Heimdall': 1 } },
+            { text: 'O dever de proteger os fracos e a honra da sua família.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 1, 'Heimdall': 1 } },
+            { text: 'A busca por desafios e a emoção de ser imprevisível.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 3, 'Heimdall': 1 } },
+            { text: 'O cumprimento rigoroso do dever de guardar a porta dos deuses.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 1, 'Heimdall': 3 } }
         ]
     },
     {
         text: 'Uma profecia anuncia o Ragnarök. Sua reação é...',
         options: [
-            { text: 'Procurar uma maneira de evitar a catástrofe.', points: { 'Frigg': 3, 'Thor': 2, 'Loki': 1 } },
-            { text: 'Se preparar para lutar até o fim.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 2 } },
-            { text: 'Tentar manipular os eventos a seu favor.', points: { 'Frigg': 2, 'Thor': 1, 'Loki': 3 } }
+            { text: 'Procurar uma maneira de evitar a catástrofe.', points: { 'Frigg': 3, 'Thor': 1, 'Loki': 1, 'Heimdall': 1 } },
+            { text: 'Se preparar para lutar até o fim.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 1, 'Heimdall': 1 } },
+            { text: 'Tentar manipular os eventos a seu favor.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 3, 'Heimdall': 1 } },
+            { text: 'Ficar de prontidão, chifre na mão, esperando o sinal de ataque.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 1, 'Heimdall': 3 } }
         ]
     },
     {
         text: 'Se você pudesse ter um poder, qual seria?',
         options: [
-            { text: 'Ver o futuro e entender as escolhas das pessoas.', points: { 'Frigg': 3, 'Thor': 1, 'Loki': 2 } },
-            { text: 'Controlar o clima e invocar trovões.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 2 } },
-            { text: 'Mudar de forma para se disfarçar e enganar.', points: { 'Frigg': 2, 'Thor': 1, 'Loki': 3 } }
+            { text: 'Ver o futuro e entender as escolhas das pessoas.', points: { 'Frigg': 3, 'Thor': 1, 'Loki': 1, 'Heimdall': 2 } },
+            { text: 'Controlar o clima e invocar trovões.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 1, 'Heimdall': 1 } },
+            { text: 'Mudar de forma para se disfarçar e enganar.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 3, 'Heimdall': 1 } },
+            { text: 'Ter sentidos tão apurados que nada no cosmos lhe escaparia.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 1, 'Heimdall': 3 } }
         ]
     },
     {
         text: 'Em uma situação de perigo, você confia...',
         options: [
-            { text: 'Na sua intuição e no seu conhecimento.', points: { 'Frigg': 3, 'Thor': 1, 'Loki': 2 } },
-            { text: 'Na sua habilidade de combate e força bruta.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 2 } },
-            { text: 'Em sua capacidade de improvisação e truques.', points: { 'Frigg': 2, 'Thor': 1, 'Loki': 3 } }
+            { text: 'Na sua intuição e no seu conhecimento.', points: { 'Frigg': 3, 'Thor': 1, 'Loki': 2, 'Heimdall': 1 } },
+            { text: 'Na sua habilidade de combate e força bruta.', points: { 'Frigg': 1, 'Thor': 3, 'Loki': 1, 'Heimdall': 1 } },
+            { text: 'Em sua capacidade de improvisação e truques.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 3, 'Heimdall': 1 } },
+            { text: 'Na sua percepção de longo alcance para reagir antes de ser atingido.', points: { 'Frigg': 1, 'Thor': 1, 'Loki': 1, 'Heimdall': 3 } }
         ]
     }
 ];
